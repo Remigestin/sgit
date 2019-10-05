e21320cf88d7e3cb5423569b0d7f8f6b9d7be3c9
Gestin <gestin.remi@gmail.com>
2019-10-05 17:39:49 +0200

    update sgit init

--- src/main/scala/app/Repo.scala
+++ src/main/scala/app/Repo.scala
@@ -1,13 +1,21 @@
 package app
 
-import java.io.File
+import java.io.{File, PrintWriter}
 
 object Repo {
 
   def init(): String = {
     val dirs = List(".sgit", ".sgit/branches", ".sgit/tags", ".sgit/commits", ".sgit/trees", ".sgit/blobs")
+    // create the dirs ans the file HEAD
     dirs.foreach(folder => new File(folder).mkdir())
     new File(".sgit/HEAD").createNewFile()
+
+    //init the file HEAD with the future branch master
+    val pw = new PrintWriter(new File(".sgit/HEAD" ))
+    pw.write("branches/master")
+    pw.close()
+
+    //return success message
     "Initialized empty Sgit repository in " + System.getProperty("user.dir") + File.separator + ".sgit" + File.separator
   }
 
