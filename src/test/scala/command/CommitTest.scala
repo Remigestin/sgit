4e163d08cf170f3153968d753dd6589d6efe6dcc
Gestin <gestin.remi@gmail.com>
2019-10-13 18:13:13 +0200

    Command Tag + test Tag + test Commit

--- src/test/scala/command/CommitTest.scala
+++ src/test/scala/command/CommitTest.scala
@@ -3,7 +3,8 @@ package command
 import java.io.File
 
 import org.scalatest.{BeforeAndAfterEach, FlatSpec, FunSuite}
-import util.FileUtil
+import util.BranchUtil.getCurrentBranchPath
+import util.{CommitUtil, FileUtil}
 
 import scala.reflect.io.Directory
 
@@ -12,9 +13,11 @@ class CommitTest extends FlatSpec with BeforeAndAfterEach {
   //init an sgit repo and .test repo before each test
   override def beforeEach(): Unit = {
     Repo.init(System.getProperty("user.dir"))
+    val repoPath = Repo.getRepoPath(System.getProperty("user.dir")).get
     new File(".test").mkdir()
     FileUtil.editFile(".test" + File.separator + "test", "Hello World", append = true)
     FileUtil.editFile(".test" + File.separator + "test2", "hello, world", append = true)
+    Add.add(repoPath,Seq(".test" + File.separator + "test"))
   }
 
   //delete all files created in the .sgit and .test directory after each test
@@ -26,22 +29,42 @@ class CommitTest extends FlatSpec with BeforeAndAfterEach {
     new Directory(new File(".test")).deleteRecursively()
   }
 
-  "A Commit" should "create the commit tree" in {
+  "The command commit"  should "create the branch in .sgit/branches if it is its first commit" in {
+    val repoPath = Repo.getRepoPath(System.getProperty("user.dir")).get
+    val pathBranch = getCurrentBranchPath(repoPath)
+    assert(!new File(pathBranch).exists())
+    Commit.commit(repoPath,"commit")
+    assert(new File(pathBranch).exists())
   }
 
-  it should "create the branch in .sgit/branches if it is its first commit" in {
-  }
+  it should "not create commit object if the previous commit is the same" in {
+    val repoPath = Repo.getRepoPath(System.getProperty("user.dir")).get
+    Commit.commit(repoPath, "commit")
+    val shaCommitObject = CommitUtil.getLastCommitObject(repoPath)
+    Commit.commit(repoPath, "commit 2")
+    val shaCommitObject2 = CommitUtil.getLastCommitObject(repoPath)
 
-  it should "not create commit tree if the previous commit is the same" in {
+    assert(shaCommitObject == shaCommitObject2)
+
   }
 
   it should "create commit in .sgit/objects with the right content" in {
   }
 
-  it should "create all trees and blobs of the commit tree in .sgit/objects" in {
+  it should "create all trees of the commit tree in .sgit/objects" in {
   }
 
   it should "update the current branch with the commit" in {
+    val repoPath = Repo.getRepoPath(System.getProperty("user.dir")).get
+    Commit.commit(repoPath, "commit")
+    val shaCommitObject = CommitUtil.getLastCommitObject(repoPath)
+
+    Add.add(repoPath, Seq(".test" + File.separator + "test2"))
+
+    Commit.commit(repoPath, "commit 2")
+    val shaCommitObject2 = CommitUtil.getLastCommitObject(repoPath)
+
+    assert(shaCommitObject != shaCommitObject2)
   }
 
 }
