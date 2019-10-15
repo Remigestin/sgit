304b5dc62a40a06137f48df7782a872f662d3c5a
Gestin <gestin.remi@gmail.com>
2019-10-15 23:31:23 +0200

    Branch

--- src/main/scala/command/Branch.scala
+++ src/main/scala/command/Branch.scala
@@ -1,8 +1,28 @@
 package command
 
+import java.io.File
+
+import util.{CommitUtil, FileUtil}
+
 object Branch {
 
-  def branch(repoPath: String): Unit = {
+  def branch(repoPath: String, name:String): String = {
+
+    if (CommitUtil.isThereACommit(repoPath)) {
+      val shaCommit = CommitUtil.getLastCommitObject(repoPath)
+      val pathBranch = repoPath + File.separator + ".sgit" + File.separator + "branches" + File.separator + name
+      if (!new File(pathBranch).exists()) {
+        FileUtil.editFile(pathBranch, shaCommit, append = false)
+      } else {
+        return "branch " + name + " already exists"
+      }
+      return "branch " + name + " successfully created"
+    }
+    "Failed to resolve 'Head' as a valid ref"
+
+  }
+
+  def branchAV(repoPath: String): Unit = {
 
   }
 
